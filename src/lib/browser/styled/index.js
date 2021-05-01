e7049ce9fa5a25f097d54b5d5ee30eae7fbaf203
faboosh <fabian.johansson@willandskill.se>
2021-05-01 15:23:09 +0200

    extended theming support

---
+++ src/lib/browser/styled/index.js
@@ -0,0 +1,73 @@
+import styled, { css } from "styled-components"
+
+export const ChatWindowView = styled.div`
+    ${({ theme }) => theme['ChatWindow'] ?? "" };
+`
+
+export const ChatMessageListView = styled.div`
+    ${({ theme }) => theme['ChatMessageList'] ?? "" };
+`
+
+export const ChatMessageView = styled.div`
+    ${({ theme }) => theme['ChatMessage']['style'] ?? "" };
+`
+
+export const ChatMessageContent = styled.span`
+    ${({ theme }) => theme['ChatMessage']['Content'] ?? "" };
+`
+
+export const ChatMessageAuthor = styled.p`
+    ${({ theme }) => theme['ChatMessage']['Author'] ?? "" };
+`
+
+export const ChatMessageInputView = styled.div`
+    ${({ theme }) => theme['ChatMessageInput'] ?? "" };
+`
+
+export const ChatMessageInputButtonGroup= styled.div`
+    ${({ theme }) => theme['ChatMessageInputButtonGroup'] ?? "" };
+`
+
+export const EmojiButton = styled.button`
+    ${({ theme }) => theme['EmojiButton'] ?? "" };
+`
+
+export const EmojiWrapper = styled.div`
+    ${({ theme }) => theme['EmojiWrapper'] ?? "" };
+`
+
+export const EmojiTopbar = styled.div`
+    ${({ theme }) => theme['EmojiTopbar'] ?? "" };
+`
+
+export const EmojiBody = styled.div`
+    ${({ theme }) => theme['EmojiBody'] ?? "" };
+`
+
+export const EmojiItem = styled.div`
+    ${({ theme }) => theme['EmojiItem'] ?? "" };
+`
+
+export const IsTypingList = styled.div`
+    ${({ theme }) => theme['IsTypingList'] ?? "" };
+` 
+
+export const IsTypingItem = styled.div`
+    ${({ theme }) => theme['IsTypingItem'] ?? "" };
+`
+
+export const ResizeableInputDiv = styled.div.attrs({
+    contentEditable: true,
+    className: "ws-chat__input"
+})`
+    ${({ theme }) => theme['ResizeableInput'] ?? "" };
+
+    &:focus {
+        outline: none;
+    }
+
+    &[placeholder]:empty:before {
+        content: attr(placeholder);
+        color: #444;
+    }
+`
--- src/styles/chatTheme.js
+++ src/styles/chatTheme.js
@@ -1,13 +1,197 @@
-import styled, { css } from 'styled-components'
+import { css } from 'styled-components'
+import { keyframes } from 'styled-components'
 
-export default theme = {
-    message: css`
+const isTypingAnim = keyframes `
+    0% { opacity: 0.5; }
+    50% { opacity: 1; }
+    100% { opacity: 0.5; }
+`
 
-    `,
-    messageList: css`
+export const accent = "#18b583"
+
+const scrollbar = css`
+    &::-webkit-scrollbar {
+        width: 12px;
+    }
     
+    &::-webkit-scrollbar-track {
+        background: #333;
+    }
+    
+    &::-webkit-scrollbar-thumb {
+        border-radius: 5px;
+        background: #555;
+    }
+`
+
+const dropShadow = css`
+    box-shadow: 0 0 5px 10px rgba(0, 0, 0, 0.1);
+`
+
+const emojiTheme = {
+    EmojiButton: css`
+        outline: none;
+        border: none;
+        background: ${accent};
+        color: white;
+        padding: 0 1rem;
+        cursor: pointer;
+        position: relative;
     `,
-    window: css`
-        
-    `
+
+    EmojiWrapper: css`
+        height: 500px;
+        width: 300px;
+        background: #333;
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(-20px, calc(-100% - 10px));
+        border-radius: 15px;
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+        ${dropShadow};
+    `,
+
+    EmojiTopbar: css`
+        width: 100%;
+        background: ${accent};
+        display: flex;
+        flex-wrap: nowrap;
+        box-sizing: border-box;
+        padding: .5rem;
+    `,
+
+    EmojiBody: css`
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        flex-grow: 1;
+        padding: .75rem;
+        box-sizing: border-box;
+        overflow-y: auto;
+        ${scrollbar};
+    `,
+
+    EmojiItem: css`
+        flex-grow: 1;
+        text-align: center;
+        padding: .3rem;
+        cursor: pointer;
+    `,
+}
+
+export default {
+    default: {
+        ChatMessage: {
+            style: css`
+                display: flex;
+                flex-direction: column;
+                ${({ messageByMe }) => messageByMe ? "align-self: flex-end;" : ""};     
+            `,
+            Content: css`
+                display: inline-block;
+                background: ${accent};
+                padding: .7em .5em;
+                margin-bottom: .7em;
+                max-width: 70vw;
+                word-break: break-word;
+                border-radius: 5px;    
+                ${({ messageByMe }) => messageByMe ? "align-self: flex-end;" : ""}; 
+                white-space: pre;
+            `,
+            Author: css`
+                opacity: .3;
+                margin: 0;
+                margin-bottom: .2em;
+                font-size: .9rem;
+                ${({ messageByMe }) => messageByMe ? "text-self: right;" : ""};
+            `,
+        },
+
+        ChatMessageList: css`
+            padding: .5rem;
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            overflow-y: auto;
+            flex-grow: 1;
+
+            ${scrollbar};
+        `,
+
+        ChatWindow: css`
+            display: flex;
+            flex-direction: column;
+            background: hsl(0, 0%, 95%);
+            padding: 0rem;
+            color: white;
+            box-sizing: border-box;
+            font-family: sans-serif;
+            background: #242424;
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;  
+        `,
+
+        ChatMessageInput: css`
+            display: flex;
+            justify-self: flex-end;
+            flex-wrap: nowrap;
+            min-height: 50px;
+            background: #333;
+            position: relative;
+            align-items: center;
+            justify-content: center;
+            z-index: 50;
+
+            ${dropShadow};
+    
+            .ws-chat__input {
+                flex-grow: 1;
+                padding: .5rem 1rem;
+            }
+    
+            .ws-chat__submit-btn {
+                outline: none;
+                border: none;
+                background: ${accent};
+                color: white;
+                padding: 0 1rem;
+                cursor: pointer;
+                font-size: 1.25rem;
+                vertical-align: middle;
+            }
+        `,
+
+        ChatMessageInputButtonGroup: css`
+            display: flex;
+            justify-self: flex-end;
+            flex-wrap: nowrap;
+            height: calc(100% - 1rem);
+            overflow: hidden;
+            border-radius: 15px;
+            margin: .5rem;
+        `,
+
+        IsTypingList: css`
+            position: fixed;
+            left: 10px;
+            bottom: 70px;
+            opacity: 0.7;
+            z-index: 1000;
+        `,
+
+        IsTypingItem: css`
+            animation-name: ${isTypingAnim};
+            animation-duration: 3s;
+            animation-iteration-count: infinite;
+        `,
+
+        ...emojiTheme,
+
+    }
 }
